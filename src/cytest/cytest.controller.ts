9dfbfcce19717ffbfe48f662942fdd18c0ffcd05
Paul Gung <gongshaoxu@gmail.com>
2023-12-28 15:27:54 +0800

    update

--- src/cytest/cytest.controller.ts
+++ src/cytest/cytest.controller.ts
@@ -7,9 +7,11 @@ import {
   Param,
   Delete,
   Put,
+  Res,
 } from '@nestjs/common';
 import { CytestService } from './cytest.service';
 import { Cytest } from './entities/cytest.entity';
+import { Response } from 'express';
 
 @Controller('cytests')
 export class CytestController {
@@ -41,5 +43,15 @@ export class CytestController {
   @Delete(':id')
   remove(@Param('id') id: number): Promise<void> {
     return this.cytestService.remove(id);
+  }
+
+  @Get('/run/:id')
+  async runCode(@Param('id') id: number, @Res() response: Response) {
+    try {
+      await this.cytestService.processCodeBlock(id);
+      response.status(200).send('代码执行成功');
+    } catch (error) {
+      response.status(500).send(`代码执行错误: ${error.message}`);
+    }
   }
 }
--- src/cytest/cytest.service.ts
+++ src/cytest/cytest.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cytest } from './entities/cytest.entity';
+import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
+import * as path from 'path';
+import { exec } from 'child_process';
 
 @Injectable()
 export class CytestService {
@@ -23,6 +27,7 @@ export class CytestService {
 
   // 根据 ID 查找单个 Cytest 记录
   async findOne(id: number): Promise<Cytest> {
+    console.log('id', id);
     return this.cytestRepository.findOne({ where: { id } });
   }
 
@@ -35,5 +40,44 @@ export class CytestService {
   // 删除 Cytest 记录
   async remove(id: number): Promise<void> {
     await this.cytestRepository.delete(id);
+  }
+
+  // 获取代码块
+  async getCodeBlockById(id: number): Promise<string | null> {
+    const cytest = await this.cytestRepository.findOne({ where: { id } });
+    return cytest ? cytest.code_block : null;
+  }
+
+  // 写入本地文件
+  async writeCodeToFile(code: string): Promise<string> {
+    const filename = uuidv4() + '.js';
+    const filePath = path.join(__dirname, '..', 'code', filename);
+    fs.writeFileSync(filePath, code);
+    return filePath;
+  }
+
+  // 跑代码
+  async runCypressTest(filePath: string): Promise<void> {
+    const command = `npx cypress run --spec "${filePath}"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行错误: ${error}`);
+        return;
+      }
+      console.log(`标准输出: ${stdout}`);
+      console.error(`标准错误: ${stderr}`);
+    });
+  }
+
+  //  汇总方法
+  async processCodeBlock(id: number): Promise<void> {
+    const codeBlock = await this.getCodeBlockById(id);
+    console.log('codeBlock', codeBlock);
+    if (codeBlock) {
+      const filePath = await this.writeCodeToFile(codeBlock);
+      await this.runCypressTest(filePath);
+    } else {
+      console.log('没有找到对应的 code_block');
+    }
   }
 }
