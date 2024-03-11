463cbb77d2e96474ce5bf13df8a3d3c54e6e2793
Paul Gung <gongshaoxu@gmail.com>
2024-03-11 14:22:55 +0800

    链路追踪

---
+++ src/tracer.ts
@@ -0,0 +1,58 @@
+'use strict';
+
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { Resource } from '@opentelemetry/resources';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+
+// Configure the SDK to export telemetry data to the console
+// Enable all auto-instrumentations from the meta package
+const exporterOptions = {
+  // url: 'http://localhost:4318/v1/traces', // http
+  url: 'http://localhost:4317', // grpc
+};
+
+// const traceExporter = new OTLPTraceExporter(exporterOptions); // http
+const traceExporter = new OTLPTraceExporter(exporterOptions); // grpc
+
+const consoleSpanExporter = new ConsoleSpanExporter();
+// const spanProcessor = new SimpleSpanProcessor(consoleSpanExporter);
+
+const spanProcessor = new SimpleSpanProcessor(traceExporter);
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [
+    // getNodeAutoInstrumentations(),
+    new NestInstrumentation(),
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+  ],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'gsx-nest',
+  }),
+});
+
+// initialize the SDK and register with the OpenTelemetry API
+// this enables the API to record telemetry
+sdk.start();
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
+
+export default sdk;
